7c914a3274d76b02c2e5296bb8e31062403553c9
VitaliiTk <tka4uk.gt41@gmail.com>
2025-02-07 12:45:22 +0600

    instal and setup morgan

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import 'dotenv/config'
+import morgan from 'morgan'
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.send('root endpoint')
@@ -13,8 +16,8 @@ app.get('/cars', (req, res) => {
   res.send('cars')
 })
 
-app.get('users', (req, res) => {
-  console.log('users')
+app.get('/users', (req, res) => {
+  res.send('users')
 })
 
 app.listen(PORT, console.log(`Server runing on port: http://localhost:${PORT}`))
